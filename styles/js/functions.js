3cbb1a7f80ba9c960d5365b7c97aac10115dad6f
Pérez Araque Ernesto <ernesto.perezaraque@alpiq.com>
2020-04-09 12:42:38 +0200

    adding white labeling to design tokens

--- styles/js/functions.js
+++ styles/js/functions.js
@@ -19,7 +19,9 @@ for (i = 0; i < x.length; i++) {
 	/*for each element, create a new DIV that will act as the selected item:*/
 	a = document.createElement('DIV');
 	a.setAttribute('class', 'select-selected');
+	a.setAttribute('value', selElmnt.options[selElmnt.selectedIndex].value);
 	a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+
 	x[i].appendChild(a);
 	/*for each element, create a new DIV that will contain the option list:*/
 	b = document.createElement('DIV');
@@ -28,6 +30,7 @@ for (i = 0; i < x.length; i++) {
 		/*for each option in the original select element,
     create a new DIV that will act as an option item:*/
 		c = document.createElement('DIV');
+		c.setAttribute('value', selElmnt.options[j].value);
 		c.innerHTML = selElmnt.options[j].innerHTML;
 		c.addEventListener('click', function (e) {
 			/*when an item is clicked, update the original select box,
@@ -39,6 +42,7 @@ for (i = 0; i < x.length; i++) {
 				if (s.options[i].innerHTML == this.innerHTML) {
 					s.selectedIndex = i;
 					h.innerHTML = this.innerHTML;
+					s.value = this.value;
 					y = this.parentNode.getElementsByClassName('same-as-selected');
 					for (k = 0; k < y.length; k++) {
 						y[k].removeAttribute('class');
@@ -123,25 +127,22 @@ const darkmodeHandler = () => {
 	darkmodeListener();
 }
 
+
 window.addEventListener('DOMContentLoaded', () => {
 	darkmodeListener();
+	designTokens();
 });
 
 const darkmodeListener = () => {
 	data = localStorage.getItem('dark-theme');
-
 	console.log(data)
-
 	if (data === 'true') {
-
 		console.log('true')
-
 		document.documentElement.style.setProperty('--alpq-color-contrast', '#353535');
 		document.documentElement.style.setProperty('--alpq-color-background', '#2d2d2d');
 		document.documentElement.style.setProperty('--alpq-font-color-primary', '#eaeaea');
 		document.documentElement.style.setProperty('--alpq-color-highlight', '#303d42');
 		document.documentElement.style.setProperty('--alpq-shadow-s', '0 0 16px rgba(0, 0, 0, 0.3)');
-
 		sidebar = document.querySelectorAll('.alpq-main-sidebar > a > div > img');
 		sidebar.forEach(() => {
 			sidebar;
@@ -149,15 +150,12 @@ const darkmodeListener = () => {
 				i.style.filter = 'grayscale(1) invert(1)';
 			});
 		});
-
 	} else {
-
 		document.documentElement.style.setProperty('--alpq-color-contrast', '');
 		document.documentElement.style.setProperty('--alpq-color-background', '');
 		document.documentElement.style.setProperty('--alpq-font-color-primary', '');
 		document.documentElement.style.setProperty('--alpq-color-highlight', '');
 		document.documentElement.style.setProperty('--alpq-shadow-s', '');
-
 		sidebar = document.querySelectorAll('.alpq-main-sidebar > a > div > img');
 		sidebar.forEach(() => {
 			sidebar;
@@ -165,18 +163,105 @@ const darkmodeListener = () => {
 				i.style.filter = 'grayscale(0) invert(0)';
 			});
 		});
-
-
 	}
 }
 
 let showDark = false
 
-const color = document.querySelector('.selectedcolor');
 
-const changeHandler = () => {
-	console.log(color.value);
-	document.documentElement.style.setProperty('--alpq-color-primary', color.value);
-};
 
-color.addEventListener('change', changeHandler);
+const designTokens = () => {
+
+	const select = document.querySelector(".select-selected");
+
+	const vare = select.addEventListener('click', () => {
+		const sa = select.innerHTML.toLowerCase()
+
+		fetch('http://127.0.0.1:5500/api/design-totkens-' + sa + '.json')
+			.then(res => {
+				return res.json()
+			}).then(json => {
+
+				console.log('success!', json);
+				const brand = json
+
+				// Background color
+
+				document.documentElement.style.setProperty('--alpq-color-primary', brand.colors.primary);
+				document.documentElement.style.setProperty('--alpq-color-contrast', brand.colors.contrast);
+				document.documentElement.style.setProperty('--alpq-color-secondary', brand.colors.secondary);
+				document.documentElement.style.setProperty('--alpq-color-highlight', brand.colors.highlight);
+				document.documentElement.style.setProperty('--alpq-color-error', brand.colors.error);
+				document.documentElement.style.setProperty('--alpq-color-success', brand.colors.success);
+				document.documentElement.style.setProperty('--alpq-color-H4', brand.colors.H4);
+				document.documentElement.style.setProperty('--alpq-color-link-hover', brand.colors.linkHover);
+				document.documentElement.style.setProperty('--alpq-color-helper', brand.colors.helper);
+				document.documentElement.style.setProperty('--alpq-color-link', brand.colors.link);
+				document.documentElement.style.setProperty('--alpq-color-inactive', brand.colors.inactive);
+				document.documentElement.style.setProperty('--alpq-color-background', brand.colors.background);
+
+				// Font Colors
+				document.documentElement.style.setProperty('--alpq-font-color-primary', brand.fontColors.primary);
+				document.documentElement.style.setProperty('--alpq-font-color-contrast', brand.fontColors.contrast);
+				document.documentElement.style.setProperty('--alpq-font-color-secondary', brand.fontColors.secondary);
+				document.documentElement.style.setProperty('--alpq-font-color-highlight', brand.fontColors.highlight);
+				document.documentElement.style.setProperty('--alpq-font-color-error', brand.fontColors.error);
+				document.documentElement.style.setProperty('--alpq-font-color-success', brand.fontColors.success);
+				document.documentElement.style.setProperty('--alpq-font-color-H4', brand.fontColors.H4);
+				document.documentElement.style.setProperty('--alpq-font-color-link-hover', brand.fontColors.linkHover);
+				document.documentElement.style.setProperty('--alpq-font-color-helper', brand.fontColors.helper);
+				document.documentElement.style.setProperty('--alpq-font-color-link', brand.fontColors.link);
+				document.documentElement.style.setProperty('--alpq-font-color-inactive', brand.fontColors.inactive);
+				document.documentElement.style.setProperty('--alpq-font-color-background', brand.fontColors.background);
+
+				document.documentElement.style.setProperty('--alpq-borderadius', brand.border.borderadius);
+
+				//Logo
+				document.querySelector('.alpq-header-logo').src = brand.logo.url
+
+				if (brand.colors.primary === '#000') {
+					
+					document.querySelector('.alpq-header').style.backgroundColor = '#ffffff';
+					document.documentElement.style.setProperty('--alpq-font-color-contrast', brand.fontColors.primary);
+
+					document.querySelector('.alpq-header-icon').style.filter = 'grayscale(1)';
+					
+					sidebar = document.querySelectorAll('.alpq-main-sidebar > a > div > img');
+					sidebar.forEach(() => {
+						sidebar;
+						sidebar.forEach((i) => {
+							i.style.filter = 'grayscale(1)';
+						});
+					});
+
+				} else{
+					document.querySelector('.alpq-header').style.backgroundColor = brand.colors.primary;
+
+					document.querySelector('.alpq-header-icon').style.filter = 'grayscale(0)';
+
+					sidebar = document.querySelectorAll('.alpq-main-sidebar > a > div > img');
+					sidebar.forEach(() => {
+						sidebar;
+						sidebar.forEach((i) => {
+							i.style.filter = 'grayscale(0)';
+						});
+					});
+
+				}
+
+				sidebar = document.querySelectorAll('.alpq-main-sidebar > a > div > img');
+					sidebar.forEach(() => {
+						sidebar;
+						sidebar.forEach((i) => {
+							i.style.filter = 'grayscale(1)';
+						});
+					});
+
+			}).catch(err => {
+				// There was an error
+				console.warn('Something went wrong.', err);
+			});
+	});
+}
+
+
